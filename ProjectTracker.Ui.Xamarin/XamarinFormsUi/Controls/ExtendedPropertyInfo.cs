2598ef61d37e308d959518e9b0a3d06dfc959837
cedric <cedric.conte@gmail.com>
2017-10-19 19:08:25 +0200

    Added ExtendedPropertyInfo

---
+++ ProjectTracker.Ui.Xamarin/XamarinFormsUi/Controls/ExtendedPropertyInfo.cs
@@ -0,0 +1,27 @@
+﻿using Csla.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormsUi.Controls
+{
+    public class ExtendedPropertyInfo : PropertyInfo
+    {
+        public ExtendedPropertyInfo()
+        {
+        }
+
+        public static readonly BindableProperty ExtraInfoProperty = BindableProperty.Create(nameof(ExtraInfo), typeof(object), typeof(ExtendedPropertyInfo), null);
+        public object ExtraInfo
+        {
+            get { return GetValue(ExtraInfoProperty); }
+            set
+            {
+                SetValue(ExtraInfoProperty, value);
+            }
+        }
+    }
+}
